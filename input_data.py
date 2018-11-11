4281f03eab4ac531a29b15b92a1703be92875ff6
mma <mma137421@gmail.com>
2018-11-12 00:52:25 +0330

    Implements CNN on cifar

--- input_data.py
+++ input_data.py
@@ -3,7 +3,18 @@ import numpy as np
 
 
 class DataSet:
-    def one_hot(self, labels):
+    def __init__(self, data, labels, one_hot=False):
+        self.images = np.array(data, ndmin=2)
+        if one_hot:
+            self.labels = self.__one_hot(labels)
+        else:
+            self.labels = labels
+
+        self.num_examples = len(data)
+        self.__index = 0
+
+    @staticmethod
+    def __one_hot(labels):
         new_label = []
         for label in labels:
             row = []
@@ -15,31 +26,15 @@ class DataSet:
             new_label.append(row)
         return np.array(new_label, ndmin=2)
 
-
-class TrainSet(DataSet):
-    def __init__(self, data, labels, one_hot=False):
-        self.images = np.array(data, ndmin=2)
-        if one_hot:
-            self.labels = super().one_hot(labels)
-        else:
-            self.labels = labels
-
-        self.num_examples = len(data)
-        self.__index = 0
-
     def next_batch(self, batch_size):
+        if self.__index + batch_size > self.images.shape[0]:
+            self.__index = 0
         x, y = self.images[self.__index: self.__index + batch_size], self.labels[self.__index: self.__index + batch_size]
-        self.__index += batch_size
+        self.__index = (self.__index + batch_size) % self.images.shape[0]
         return x, y
 
-
-class TestSet(DataSet):
-    def __init__(self, data, labels, one_hot=False):
-        self.images = np.array(data, ndmin=2)
-        if one_hot:
-            self.labels = super().one_hot(labels)
-        else:
-            self.labels = labels
+    def normalize(self):
+        self.images = np.array(list(map(lambda x: x/255, self.images)))
 
 
 class Cifar:
@@ -50,8 +45,13 @@ class Cifar:
             for i in range(len(batch[b'data'])):
                 data.append(batch[b'data'][i])
                 labels.append(batch[b'labels'][i])
-        self.train = TrainSet(data, labels, one_hot)
-        self.test = TestSet(test[b'data'], test[b'labels'], one_hot)
+
+        self.train = DataSet(data[:int(4 * len(data) / 5)], labels[:int(4 * len(data) / 5)], one_hot)
+        self.validation = DataSet(data[int(4 * len(data) / 5):], labels[int(4 * len(data) / 5):], one_hot)
+        self.test = DataSet(test[b'data'], test[b'labels'], one_hot)
+
+    def normalize_data(self):
+        self.train.normalize()
 
 
 def read_data_sets(url, one_hot=False):
